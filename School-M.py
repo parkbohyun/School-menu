89775c7dfab8904b5342a9134bc9d421a47049c1
Bo._.H <47629333+parkbohyun@users.noreply.github.com>
2023-04-27 11:25:50 +0900

    Update School-M.py

--- School-M.py
+++ School-M.py
@@ -28,15 +28,4 @@ def main():
     print("[ 성일정보고등학교 급식 ]"+"\n"+DD_day+" ("+(W_day[WN_day])+")\n[중식]")
     print(meal)
     
-    # 새롭게 추가할 학교
-    scinfo = neis.schoolInfo(SCHUL_NM=name)     # 학교이름으로 학교정보를 요청하고 교육청코드 와 학교코드로 가져옵니다.
-    AE = scinfo[0].ATPT_OFCDC_SC_CODE       # 교육청코드
-    SE = scinfo[0].SD_SCHUL_CODE        # 학교코드
-
-    scmeal = neis.mealServiceDietInfo(AE, SE, MLSV_YMD=D_day)         # 학교코드와 교육청 코드로 2022년 04월 01일의 급식 정보 요청
-    meal = scmeal[0].DDISH_NM.replace("<br/>", "\n")  # 줄바꿈으로 만든뒤 출력
-
-    print("[ 고등학교 급식 ]"+"\n"+DD_day+" ("+(W_day[WN_day])+")\n[중식]")
-    print(meal)
-
 main()
